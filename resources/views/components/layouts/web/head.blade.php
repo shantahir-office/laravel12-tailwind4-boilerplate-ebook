f138a7814c55f223602730772186f2e404feddf3
shantahir-office <shantahiroffice23@gmail.com>
2026-01-07 21:18:05 +0500

    1. fixed head

--- resources/views/components/layouts/web/head.blade.php
+++ resources/views/components/layouts/web/head.blade.php
@@ -1,7 +1,7 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
-<title>{{ $title ?? 'Trademark' }}</title>
+<title>{{ $title ?? 'Book' }}</title>
 <meta name="description" content="{{ $description ?? '' }}">
 <meta name="keywords" content="{{ $keywords ?? '' }}">
 {{-- <link rel="icon" type="images/jpg" href="{{ asset('assets/img/fav.svg') }}"> --}}
